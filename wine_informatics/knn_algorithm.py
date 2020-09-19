d5680c45a4c28f7b409797198303e69d566cf70b
Williano <paawilly17@gmail.com>
2020-09-19 18:42:17 -0500

    Add new dependencies

---
+++ wine_informatics/knn_algorithm.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+
+def load_dataset():
+    pass
+
+def process_dataset():
+    pass
+
+def knn_algorithm():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

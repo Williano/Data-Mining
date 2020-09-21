7db476394816e92fd03b2412cb65876de03aa1ca
Williano <paawilly17@gmail.com>
2020-09-21 15:31:07 -0500

    Add k fold cross validation

--- wine_informatics/knn_algorithm/knn_algorithm.py
+++ wine_informatics/knn_algorithm/knn_algorithm.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.utils import shuffle
 
 
@@ -19,7 +20,7 @@ def analyse_dataset(processed_data):
     print(data.head())
 
 
-def knn_algorithm(wine_dataset):
+def knn_algorithm_with_holdout_validation(wine_dataset):
 
     # shuffled_data = shuffle(wine_dataset)
     # shuffled_data.reset_index(inplace=True, drop=True)
@@ -28,33 +29,56 @@ def knn_algorithm(wine_dataset):
     dataset = wine_dataset.iloc[:, : 486].values
 
 
-    X_train, X_test, y_train, y_test = train_test_split(dataset, label, test_size=0.20)
+    X_train, X_test, y_train, y_test = train_test_split(dataset, label, test_size=0.20, random_state=1, stratify=label)
 
 
-    classifier = KNeighborsClassifier(n_neighbors=30)
+    classifier = KNeighborsClassifier(n_neighbors=37)
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
 
+    accuracy = accuracy_score(y_test, y_pred) * 100
+    print(accuracy)
+
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
-    error = []
+    # error = []
 
-    # Calculating error for K values between 1 and 40
-    for i in range(1, 50):
-        knn = KNeighborsClassifier(n_neighbors=i)
-        knn.fit(X_train, y_train)
-        pred_i = knn.predict(X_test)
-        error.append(np.mean(pred_i != y_test))
+    # # Calculating error for K values between 1 and 40
+    # for i in range(1, 60):
+    #     knn = KNeighborsClassifier(n_neighbors=i)
+    #     knn.fit(X_train, y_train)
+    #     pred_i = knn.predict(X_test)
+    #     error.append(np.mean(pred_i != y_test))
 
-    plt.figure(figsize=(12, 6))
-    plt.plot(range(1, 50), error, color='red', linestyle='dashed', marker='o',
-            markerfacecolor='blue', markersize=10)
-    plt.title('Error Rate K Value')
-    plt.xlabel('K Value')
-    plt.ylabel('Mean Error')
-    #plt.show()
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(range(1, 60), error, color='red', linestyle='dashed', marker='o',
+    #         markerfacecolor='blue', markersize=10)
+    # plt.title('Error Rate K Value')
+    # plt.xlabel('K Value')
+    # plt.ylabel('Mean Error')
+    # plt.show()
+
+
+def knn_algorithm_with_k_fold_validation(wine_dataset):
+
+    shuffled_data = shuffle(wine_dataset)
+    shuffled_data.reset_index(inplace=True, drop=True)
+
+    # Extract features and label
+    label = shuffled_data["Class"].values
+    dataset = shuffled_data.iloc[:, : 486].values
+
+    # Create classifier
+    knn_classifier = KNeighborsClassifier(n_neighbors=30)
+
+    # Train model with 10 fold cross validation
+    cross_validation_scores = cross_val_score(knn_classifier, dataset, label, cv=10)
+
+
+    print(cross_validation_scores)
+    print("cross validation scores mean:{}".format(np.mean(cross_validation_scores)))
 
 
 def main():
@@ -65,7 +89,9 @@ def main():
 
     #analyse_dataset(processed_data_file)
 
-    knn_algorithm(processed_data_file)
+    #knn_algorithm_with_holdout_validation(processed_data_file)
+
+    knn_algorithm_with_k_fold_validation(processed_data_file)
 
 
 

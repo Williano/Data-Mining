30602a7f911b6513e6c9e0083a9b3ca347c346cc
Williano <paawilly17@gmail.com>
2020-10-06 18:21:44 -0500

    Refactor code

--- wine_informatics/knn_algorithm/knn_algorithm.py
+++ wine_informatics/knn_algorithm/knn_algorithm.py
@@ -19,65 +19,72 @@ def analyse_dataset(processed_data):
     print(data.head())
 
 
-def knn_algorithm_with_holdout_validation(wine_dataset):
+def knn_algorithm_with_holdout_validation(wine_dataset, k_value):
 
     shuffled_data = wine_dataset.sample(frac=1).reset_index(drop=True)
 
     label = wine_dataset["Class"].values
-    dataset = wine_dataset.iloc[:, : 486].values
+    #dataset = wine_dataset.iloc[:, : 486].values
+    dataset = wine_dataset.iloc[:, : 485].values
+    #dataset = wine_dataset.iloc[:, : 482].values
 
 
     X_train, X_test, y_train, y_test = train_test_split(dataset, label, test_size=0.20, random_state=1, stratify=label)
 
 
-    classifier = KNeighborsClassifier(n_neighbors=38, algorithm='auto', metric="jaccard")
+    classifier = KNeighborsClassifier(n_neighbors=k_value, algorithm='auto', metric="jaccard")
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred) * 100
+    print(f"Accuracy for {k_value} is:")
     print(accuracy)
 
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
 
-    error = []
+    print("********************************************")
+    # error = []
 
-    # Calculating error for K values between 1 and 40
-    for i in range(1, 100):
-        knn = KNeighborsClassifier(n_neighbors=i)
-        knn.fit(X_train, y_train)
-        pred_i = knn.predict(X_test)
-        error.append(np.mean(pred_i != y_test))
+    # # Calculating error for K values between 1 and 40
+    # for i in range(1, 100):
+    #     knn = KNeighborsClassifier(n_neighbors=i)
+    #     knn.fit(X_train, y_train)
+    #     pred_i = knn.predict(X_test)
+    #     error.append(np.mean(pred_i != y_test))
 
-    plt.figure(figsize=(12, 6))
-    plt.plot(range(1, 100), error, color='red', linestyle='dashed', marker='o',
-            markerfacecolor='blue', markersize=10)
-    plt.title('Error Rate K Value')
-    plt.xlabel('K Value')
-    plt.ylabel('Mean Error')
-    plt.show()
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(range(1, 100), error, color='red', linestyle='dashed', marker='o',
+    #         markerfacecolor='blue', markersize=10)
+    # plt.title('Error Rate K Value')
+    # plt.xlabel('K Value')
+    # plt.ylabel('Mean Error')
+    # plt.show()
 
 
-def knn_algorithm_with_k_fold_validation(wine_dataset):
+def knn_algorithm_with_k_fold_validation(wine_dataset, k_value):
 
     #shuffled_data = wine_dataset.sample(frac=1).reset_index(drop=True)
 
     # Extract features and label
     label = wine_dataset["Class"].values
-    dataset = wine_dataset.iloc[:, : 486].values
+    #dataset = wine_dataset.iloc[:, : 486].values
+    #dataset = wine_dataset.iloc[:, : 485].values
+    dataset = wine_dataset.iloc[:, : 482].values
 
     # Create classifier
-    knn_classifier = KNeighborsClassifier(n_neighbors=100,  algorithm='auto', metric="jaccard")
+    knn_classifier = KNeighborsClassifier(n_neighbors=k_value,  algorithm='auto', metric="jaccard")
 
     # Train model with 10 fold cross validation
     cross_validation_scores = cross_val_score(knn_classifier, dataset, label, cv=10)
 
-
+    print(f"The cross validation for {k_value} is: ")
     print(cross_validation_scores)
     print()
     print("Cross validation scores mean: {}%".format(np.mean(cross_validation_scores) * 100))
 
+    print("************************************************************************")
 
 def knn_algorithm_with_hypertuning(wine_dataset):
 
@@ -111,18 +118,26 @@ def knn_algorithm_with_hypertuning(wine_dataset):
 
 def main():
 
-    wine_dataset_file = "drink_and_hold_dataset.csv"
+    #wine_dataset_file = "drink_and_hold_dataset.csv"
 
-    processed_data_file = load_dataset(wine_dataset_file)
+    #tweaked_wine_dataset_file = "drink_and_hold_dataset_with_finish_attribute_deleted.csv"
+
+    tweaked_wine_dataset_file = "drink_and_hold_dataset_with_4_attributes_above_35_percent_deleted.csv"
+
+    processed_data_file = load_dataset(tweaked_wine_dataset_file)
 
     #analyse_dataset(processed_data_file)
 
-    knn_algorithm_with_holdout_validation(processed_data_file)
+    #knn_algorithm_with_holdout_validation(processed_data_file)
 
     #knn_algorithm_with_k_fold_validation(processed_data_file)
 
     #knn_algorithm_with_hypertuning(processed_data_file)
 
+    k_values = [20, 30, 35, 40, 45, 50, 55, 60, 65, 70, 77, 85, 95, 100]
+
+    for k in k_values:
+          knn_algorithm_with_k_fold_validation(processed_data_file, k)
 
 
 
